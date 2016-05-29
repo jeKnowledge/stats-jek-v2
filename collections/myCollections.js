9737373c1b6786ffae74aa6862d14ff3346b5ced
joelpires <joelrogao@hotmail.com>
2016-05-29 13:17:38 +0100

    collections

---
+++ collections/myCollections.js
@@ -0,0 +1,9 @@
+GitHubCollection = new Mongo.Collection('github');
+
+TwitterCollection = new Mongo.Collection('twitter');
+
+FbookCollection = new Mongo.Collection('facebook');
+
+SlackCollection = new Mongo.Collection('slack');
+
+JekPageCollection = new Mongo.Collection('jekPage');	
