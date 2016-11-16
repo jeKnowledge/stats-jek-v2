10a63d65202707966b0f3105e893dd9b90d0e115
joelpires <joelrogao@hotmail.com>
2016-11-16 17:04:01 +0000

    Info About Channels and Users Extracted

--- server/slack.js
+++ server/slack.js
@@ -1,8 +1,12 @@
 //-----------------------------------------------------  STRUCTS  ------------------------------------------------------------------
 //CLASS THAT HOLDS ALL THE INFO ABOUT SLACK STATS
 class SlackBucket {
-  constructor() {
-  }
+    constructor() {
+        this.numberMembers = 0;
+        this.members = {};
+        this.numberChannels = 0;
+        this.channels = {};
+    }
  }
 
 
@@ -16,18 +20,44 @@ Slack = {
         //BUCKET THAT CONTAINS ALL THE DATA -------------------------------------------------------------------
         let slackInfo = new SlackBucket();
 
-        //LETS EXTRACT THE BASIC INFO ----------------------------------------------------------------------
+        //LETS EXTRACT INFO ABOUT MEMBERS ----------------------------------------------------------------------
         let results;
         try{
         results = HTTP.call('GET', "https://slack.com/api/users.list?token=" + Meteor.settings.TOKEN_SLACK, {headers: {"User-Agent": "Meteor/1.0"}});
         } catch(e) {
-        console.log("AN ERROR OCURRED WHILE CALLING FOR VIMEO API: ", e);
+        console.log("AN ERROR OCURRED WHILE CALLING FOR SLACK API: ", e);
         }
-        let generalResults = JSON.parse(results.content);
+        let membersResults = JSON.parse(results.content);
+        let userID;
+        for (var i = 0; i < membersResults.members.length; i++) {
+            slackInfo.numberMembers++;
+            userID = membersResults.members[i].id;
+            slackInfo.members[userID] = {};
+            slackInfo.members[userID].username =  membersResults.members[i].name;
+            slackInfo.members[userID].name =  membersResults.members[i].profile.real_name_normalized;
+        }
 
-        console.log(generalResults);
-
-
+        //LETS EXTRACT INFO ABOUT CHANNELS ----------------------------------------------------------------------
+        try{
+        results = HTTP.call('GET', "https://slack.com/api/channels.list?token=" + Meteor.settings.TOKEN_SLACK, {headers: {"User-Agent": "Meteor/1.0"}});
+        } catch(e) {
+        console.log("AN ERROR OCURRED WHILE CALLING FOR SLACK API: ", e);
+        }
+        let channelsResults = JSON.parse(results.content);
+        let channelID;
+        for (var i = 0; i < channelsResults.channels.length; i++) {
+            slackInfo.numberChannels++;
+            channelID = channelsResults.channels[i].id;
+            slackInfo.channels[channelID] = {};
+            slackInfo.channels[channelID].name = channelsResults.channels[i].name;
+            slackInfo.channels[channelID].members = channelsResults.channels[i].members;
+            slackInfo.channels[channelID].is_archived = channelsResults.channels[i].is_archived;
+            slackInfo.channels[channelID].num_members = channelsResults.channels[i].num_members;
+            slackInfo.channels[channelID].purpose = channelsResults.channels[i].purpose.value;
+            slackInfo.channels[channelID].topic = channelsResults.channels[i].topic.value;
+            slackInfo.channels[channelID].created_at = channelsResults.channels[i].created_at; //TODO: Parse data
+        }
+        console.log(slackInfo.channels);
         SlackCollection.insert(slackInfo);
 
     },
@@ -37,3 +67,11 @@ Slack = {
     }
 
 };
+
+/*EXAMPLE AN OF OBJECT THAT HOLDS ALL THE INFO ABOUT A JEKNOWLEDGE'S MEMBERS
+    this.id = {username = "", name = ""};
+*/
+
+/*EXAMPLE AN OF OBJECT THAT HOLDS ALL THE INFO ABOUT A JEKNOWLEDGE'S MEMBERS
+    this.id = {name = "", is_active = "", members = [], num_members = 0, topic = "", purpose = "", created_at = timestamp};
+*/
