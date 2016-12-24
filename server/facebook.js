d497b6270f859df2cd87b8a993d9dfe96c9cf0a9
joelpires <joelrogao@hotmail.com>
2016-12-24 17:08:04 +0000

    basic info like number of people that are talking about jeknowledge, that like its page, etc

--- server/facebook.js
+++ server/facebook.js
@@ -2,19 +2,12 @@
 //CLASS THAT HOLDS ALL THE INFO ABOUT FACEBOOK STATS
 class FacebookBucket {
     constructor() {
-        this.members = [];
         this.pageLikes = 0;
-        this.likeNames = [];
+        this.starRating = 0.0;
+        this.talkingAbout = 0;
+        this.Events = {};
         this.totalPhotos = 0;
         this.totalVideos = 0;
-
-        this.totalLikes = 0;
-        this.totalLoves = 0;
-        this.totalLaughs = 0;
-        this.totalSads = 0;
-        this.totalAnger = 0;
-        this.totalReactions = 0
-        this.totalEvents = 0;
 
         this.postsPerDay = 0;
         this.postsPerWeek = 0;
@@ -25,14 +18,45 @@ class FacebookBucket {
         this.postsPerYear = 0;
         this.totalPosts = 0;
 
-        this.SharesPerDay = 0;
-        this.SharesPerWeek = 0;
-        this.SharesPerMonth = 0;
-        this.SharesPerThreeMonths = 0;
-        this.SharesPerSixMonths = 0;
-        this.SharesPerNineMonths = 0;
-        this.SharesPerYear = 0;
+        //TODO: -------
+        this.totalLoves = 0;
+        this.totalLaughs = 0;
+        this.totalSads = 0;
+        this.totalAnger = 0;
+        this.totalReactions = 0
+
+        this.likesPerDay = 0;
+        this.likesPerWeek = 0;
+        this.likesPerMonth = 0;
+        this.likesPerThreeMonths = 0;
+        this.likesPerSixMonths = 0;
+        this.likesPerNineMonths = 0;
+        this.likesPerYear = 0;
+        this.totalLikes = 0;
+
+        this.sharesPerDay = 0;
+        this.sharesPerWeek = 0;
+        this.sharesPerMonth = 0;
+        this.sharesPerThreeMonths = 0;
+        this.sharesPerSixMonths = 0;
+        this.sharesPerNineMonths = 0;
+        this.sharesPerYear = 0;
         this.totalShares = 0;
+
+        this.eventsPerDay = 0;
+        this.eventsPerWeek = 0;
+        this.eventsPerMonth = 0;
+        this.eventsPerThreeMonths = 0;
+        this.eventsPerSixMonths = 0;
+        this.eventsPerNineMonths = 0;
+        this.eventsPerYear = 0;
+
+        this.growthPageLikesPerWeek = 0.0;
+        this.growthPageLikesPerMonth = 0.0;
+        this.growthPageLikesPerThreeMonths = 0.0;
+        this.growthPageLikesPerSixMonths = 0.0;
+        this.growthPageLikesPerNineMonths = 0.0;
+        this.growthPageLikesPerYear = 0.0;
 
     }
  }
@@ -43,14 +67,119 @@ Facebook = {
         //BUCKET THAT CONTAINS ALL THE DATA -------------------------------------------------------------------
         let FacebookInfo = new FacebookBucket();
 
-        //LETS EXTRACT INFO ABOUT MEMBERS ----------------------------------------------------------------------
+        //LETS EXTRACT INFO ABOUT EVENTS ----------------------------------------------------------------------
         let results;
-        try{
-        results = HTTP.call('GET', "https://graph.facebook.com/" + Meteor.settings.JEKNOWLEDGE_FACEBOOK_ID + "/events?access_token=" + Meteor.settings.TOKEN_JOEL_FACEBOOK, {headers: {"User-Agent": "Meteor/1.0"}});
-        } catch(e) {
-        console.log("AN ERROR OCURRED WHILE CALLING FOR THE USERS LIST: ", e);
+        let before = "";
+        let eventsResults;
+        FacebookInfo.Events.totalNumberEvents = 0;
+        FacebookInfo.Events.totalNumberAttendees = 0;
+        while(true){
+            try{
+                results = results = HTTP.call('GET', "https://graph.facebook.com/v2.8/" + Meteor.settings.JEKNOWLEDGE_FACEBOOK_ID + "/events?access_token=" + Meteor.settings.TOKEN_JOEL_FACEBOOK + "&before=" + before, {headers: {"User-Agent": "Meteor/1.0"}});
+            } catch(e) {
+                console.log("AN ERROR OCURRED WHILE CALLING FOR FACEBOOK EVENTS: ", e);
+            }
+            eventsResults = JSON.parse(results.content);
+
+            if(typeof(eventsResults.paging) === 'undefined'){
+                break;
+            }
+
+            FacebookInfo.Events.eventsIDs = {};
+            for (var i = 0; i < eventsResults.data.length; i++) {
+                let beforeCursor = "";
+                let eventID = eventsResults.data[i].id;
+                FacebookInfo.Events.totalNumberEvents = eventsResults.data.length;
+                FacebookInfo.Events.eventsIDs[eventID] = {};
+                FacebookInfo.Events.eventsIDs[eventID].name = eventsResults.data[i].name;
+                FacebookInfo.Events.eventsIDs[eventID].startTime = eventsResults.data[i].start_time;
+                FacebookInfo.Events.eventsIDs[eventID].attending = 0;
+
+                while(true){
+                    try{
+                        results = results = HTTP.call('GET', "https://graph.facebook.com/v2.8/" + eventID + "/attending?access_token=" + Meteor.settings.TOKEN_JOEL_FACEBOOK + "&before=" + beforeCursor, {headers: {"User-Agent": "Meteor/1.0"}});
+                    } catch(e) {
+                        console.log("AN ERROR OCURRED WHILE CALLING FOR DETAILS OF THE EVENT WITH THE ID: " + eventID, e);
+                    }
+                    eventAttending = JSON.parse(results.content);
+
+                    if(typeof(eventAttending.paging) === 'undefined'){
+                        break;
+                    }
+                    for (var l = 0; l < eventAttending.data.length; l++) {
+                        if (eventAttending.data[l].rsvp_status === 'attending') {
+                            FacebookInfo.Events.eventsIDs[eventID].attending++;
+                            FacebookInfo.Events.totalNumberAttendees++;
+                        }
+                    }
+                    beforeCursor = eventAttending.paging.cursors.before;
+
+                }
+            }
+            before = eventsResults.paging.cursors.before;
         }
-        let membersResults = JSON.parse(results.content);
-        console.log(membersResults);
+
+        //LETS EXTRACT THE BASIC INFO
+        try{
+            results = results = HTTP.call('GET', "https://graph.facebook.com/v2.8/" + Meteor.settings.JEKNOWLEDGE_FACEBOOK_ID + "/?fields=fan_count, members, overall_star_rating, talking_about_count&access_token=" + Meteor.settings.TOKEN_JOEL_FACEBOOK, {headers: {"User-Agent": "Meteor/1.0"}});
+        } catch(e) {
+            console.log("AN ERROR OCURRED WHILE CALLING FOR JEKNOWLEDGE PAGE BASIC INFO: ", e);
+        }
+        basicResults = JSON.parse(results.content);
+        FacebookInfo.pageLikes = basicResults.fan_count;
+        FacebookInfo.starRating = basicResults.overall_star_rating;
+        FacebookInfo.talkingAbout = basicResults.talking_about_count;
+
+        before = "";
+        while(true){
+            try{
+                results = results = HTTP.call('GET', "https://graph.facebook.com/v2.8/" + Meteor.settings.JEKNOWLEDGE_FACEBOOK_ID + "/photos?access_token=" + Meteor.settings.TOKEN_JOEL_FACEBOOK + "&before=" + before, {headers: {"User-Agent": "Meteor/1.0"}});
+            } catch(e) {
+                console.log("AN ERROR OCURRED WHILE CALLING FOR FACEBOOK PHOTOS: ", e);
+            }
+            photosResults = JSON.parse(results.content);
+
+            if(typeof(photosResults.paging) === 'undefined'){
+                break;
+            }
+            FacebookInfo.totalPhotos++;
+
+            before = photosResults.paging.cursors.before;
+        }
+
+        before = "";
+        while(true){
+            try{
+                results = results = HTTP.call('GET', "https://graph.facebook.com/v2.8/" + Meteor.settings.JEKNOWLEDGE_FACEBOOK_ID + "/videos?access_token=" + Meteor.settings.TOKEN_JOEL_FACEBOOK + "&before=" + before, {headers: {"User-Agent": "Meteor/1.0"}});
+            } catch(e) {
+                console.log("AN ERROR OCURRED WHILE CALLING FOR FACEBOOK VIDEOS: ", e);
+            }
+            videosResults = JSON.parse(results.content);
+
+            if(typeof(videosResults.paging) === 'undefined'){
+                break;
+            }
+            FacebookInfo.totalVideos++;
+
+            before = videosResults.paging.cursors.before;
+        }
+
+        //LETS EXTRACT INFO ABOUT POSTS
+
     }
 };
+
+/*EXAMPLE AN OF OBJECT THAT HOLDS ALL THE INFO ABOUT EVENTS
+    this.eventsIDs = { this.id1 = {name = "",
+                                    attendees = ""
+                                },
+                    this.id2 = { name = "",
+                                attendees = ""
+                                },
+                    ...,
+                },
+    this.totalEvents = 0;
+    this.totalAttendees = 0;
+*/
+
+//TODO: Refresh token automatically
